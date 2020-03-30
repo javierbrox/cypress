1ae5803f41644437b05e06267419ae5b96330838
javier brox <javier_brox@PC0NKPY9.bq-ms.local>
2020-03-30 12:40:50 +0200

    changes in Search by status

--- cypress/integration/features/workflows.feature
+++ cypress/integration/features/workflows.feature
@@ -3,20 +3,16 @@ Feature: Workflows
 
   Scenario: Search by WorkflowId with runId
     Given I navigate to "Bpm-workflows"
-  #   And a workflowId
-  #   And a runId
      When I write "workflowID" workflowId
      And I write "RunId" runId
      And I click in "search" button
      Then Workflow Id is shown with every data
 
 
-  #  Scenario: Search by status
-  #    Given I navigate to "Bpm-workflows"
-  #   And a workflowId
-  #   And a runId
-  #   When I click in "arrow status box" button
-  #   Then all status are shown
-  #   When I select "status"
-  #   And I click in "search status" button
-  #   Then Workflow Id in "status" status are shown
+  Scenario: Search by status
+     Given I navigate to "Bpm-workflows"
+     When I click in "arrow status box" button
+     Then all status are shown
+     When I select "status"
+     And I click in "search status" button
+     Then Workflow Id in "status" status are shown
--- cypress/support/step_definitions/common/common.sd.js
+++ cypress/support/step_definitions/common/common.sd.js
@@ -21,26 +21,12 @@ Given("{string} input is present", (inputName) => {
   cy.inputIsPresent(inputName)
 })
 
-Then(`check basic options`, () => {
-    
-  cy.get('tbody > :nth-child(1) > :nth-child(2) > a').click()
-  cy.get('.workflow-status > dt'). contains('Status') // esto chequea algo de la pagina para ver que el paso anterior se ha hecho correctamente
-  cy.wait(250)
-  cy.get('.history').click()
-  cy.get('.export'). contains('Export')
-  cy.wait(250)
-  cy.get('.compact').click()
-  cy.get('.json').click()
-  cy.get('.date-range-picker > .dropdown > .dropdown-toggle > .selected-tag')
-  //cy.pageIsReady('https://cadence-ui.private.sta.k8s.masmovil.com/domain/provision-domain/workflows/NEW_171851_722608084/ace20041-af0f-485d-85e1-d308fe07e4bc/summary')
-  // cy.get('#search_query_top').type('dress');
-  
-});
+
 
 
 //AQUI EMPIEZAN LOS TETS NUEVOS QUE ESTOY HACIENDO
 
-
+//TEST Search by WorkflowId with runId
 
 When('I write "workflowID" workflowId', () =>{
   cy.get('#mat-input-3').type('LARA-BAJA-MIERCOLES-01')
@@ -64,6 +50,25 @@ Then('Workflow Id is shown with every data', () => {
 
 })
 
+//TEST Search by status
 
+When('I click in "arrow status box" button', () =>{
+  cy.get('.mat-select-placeholder').click()
+  
+ 
+}); 
 
+Then('all status are shown', () =>{
+  cy.get('.mat-option-text').contains('open')
 
+}); 
+
+When ('I select "status"', () =>{
+  cy.get('.mat-option-text').click()
+
+});
+
+And ('I click in "search status" button', () =>{
+  cy.get(':nth-child(2) > .col-md-2 > .mat-primary').click()
+
+});
